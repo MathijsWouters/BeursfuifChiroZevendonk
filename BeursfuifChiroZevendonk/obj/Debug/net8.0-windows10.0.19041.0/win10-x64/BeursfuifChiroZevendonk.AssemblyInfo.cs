a99d5b9820c4220739016668a30a9f57acfffd9b
Mathijs Wouters <r0938857@student.thomasmore.be>
2024-09-19 15:32:46 +0200

    l

--- BeursfuifChiroZevendonk/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/BeursfuifChiroZevendonk.AssemblyInfo.cs
+++ BeursfuifChiroZevendonk/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/BeursfuifChiroZevendonk.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BeursfuifChiroZevendonk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e21198ea222d91b390b5107f511403f793fd304a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0d8315e05466cffc9c378b5665e0f5bcc098f8e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BeursfuifChiroZevendonk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BeursfuifChiroZevendonk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
